ea89ec28dacdec31d755bce9f405a221ed422f78
simab <simab@users.noreply.github.com>
2022-11-05 20:22:36 -0400

    pdfs fixed

--- pset6_exercise.R
+++ pset6_exercise.R
@@ -4,5 +4,7 @@ library(here)
 source(here("dataverse_files","00_analysis_functions.R"))
 source(here("dataverse_files","00_install_packages.R"))
 source(here("dataverse_files","01_prepare_main_analysis_data.R"))
-source(here("dataverse_files","02_mediation_models.R"))
+#source(here("dataverse_files","02_mediation_models.R"))
+
+
 
