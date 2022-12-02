5e4ab464206bd88a273bd30042a252900ec56fc7
simab <simab@users.noreply.github.com>
2022-12-02 17:17:13 -0500

    sambanis data integrate

--- redemption-through-rebellion-dataverse_files/rep_06_claim_type.R
+++ redemption-through-rebellion-dataverse_files/rep_06_claim_type.R
@@ -8,12 +8,18 @@ grievance.escal.df = read_dta(here("sambanis_paper_rep_materials",
   select(gwgroupid, year, statusname, nviolsd_violsd, 
          lost_autonomy, downgr5_aut, downgr5_incl)  
 
+prop.type.action = grievance.escal.df %>%
+  group_by(gwgroupid) %>%
+  summarise(mean_action = mean(nviolsd_violsd)) %>%
+  drop_na()
+
 #load ruggedness analysis df
 analysis.df = read.csv(here("redemption-through-rebellion-dataverse_files","epr_segment_level_analysis_rugged.csv"))
 
 #merge grievance.escal.df with main analysis dataframe
 exten.analysis.df = analysis.df %>%
-  left_join(grievance.escal.df, by = c("gwgroupid", "year"))
+  left_join(grievance.escal.df, by = c("gwgroupid", "year")) %>%
+  left_join(prop.type.action)
 
 #write extension analysis csv
 write.csv(exten.analysis.df, 
